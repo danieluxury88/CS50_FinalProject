42586bb08f919cb9ba64aa6561685616f4ab2240
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-04-14 06:59:50 -0500

    Move javascript logic to js file, and refactor code!

--- home/static/home/front_logic.js
+++ home/static/home/front_logic.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     const progressBar = document.getElementById('day-progress-bar');
     progressBar.addEventListener('click', toggleInverted);
+    
+    const cycle_progressBar = document.getElementById('cycle-progress-bar');
+    cycle_progressBar.addEventListener('click', toggleCycleBar);
 
-    btn_toggle_work_session.onclick = toggleWorkingSession  
+    const nav_cycle_time_lbl = document.getElementById('nav_cycle_lbl_time');
+    nav_cycle_time_lbl.addEventListener('click', toggleCycleBar);
+
+    btn_toggle_work_session.onclick = toggleWorkingSession;
 
     updateNavClock();
     updateAuxClock();
+    updateNavCycleTimer();
     updateDayProgressBar();
+    updateCycleProgressBar();
     updatePeriodicData();
 
   });
 
-
-function toggleWorkingSession(){
-  event.preventDefault();
-  console.log("button");
-  fetch(`personal/toggle_work_session/` , {
-      method: 'GET',
-  });
+function updatePeriodicData () { 
+  setInterval(updateNavClock, 1000);
+  setInterval(updateAuxClock, 1000);
+  setInterval(updateDayProgressBar, 60000);
+  setInterval(updateCycleProgressBar, 60000); // Update every minute
+  setInterval(updateNavCycleTimer, 500); // Update every minute
 }
 
 function updateTimer(element_id, timer) {
@@ -25,11 +32,15 @@ function updateTimer(element_id, timer) {
   const minutes = timer.getMinutes();
   const seconds = timer.getSeconds();
   const timeString = `${hours}:${padZero(minutes)}:${padZero(seconds)}`;
-  document.getElementById(element_id).innerHTML = timeString;
+  UpdateStringTimer(element_id,timeString );
 }
 
 function padZero(number) {
   return number < 10 ? "0" + number : number;
+}
+
+function UpdateStringTimer(element_id, timeString ) {
+  document.getElementById(element_id).innerHTML = timeString;
 }
 
 function updateNavClock() {
@@ -42,54 +53,91 @@ function updateAuxClock() {
   updateTimer("timer", now );
 }
 
-function updatePeriodicData () { 
-  setInterval(updateNavClock, 1000);
-  setInterval(updateAuxClock, 1000);
-  setInterval(updateDayProgressBar, 60000);
-}
-
-function myFunction() {
-  // add your desired functionality here
-  console.log('Button clicked!');
-}
-
-// 
-
-
 let isDayBarInverted = false;
-
-function updateDayProgressBar() {
-  const progressBar = document.getElementById('day-progress-bar');
-  const progressText = document.getElementById('day_progress-text');
-  updateProgressBar(progressBar,progressText, isDayBarInverted )
-}
 
 function toggleInverted() {
   isDayBarInverted = !isDayBarInverted;
   updateDayProgressBar();
 }
 
-            
-function updateProgressBar(progressBar, progressText, isInverted) {
+function updateDayProgressBar() {
+  const progressBar = document.getElementById('day-progress-bar');
+  const progressText = document.getElementById('day_progress-text');
+  progressPercentage = calculateDayProgressBarPercentage(isDayBarInverted);
+  updateProgressBar(progressBar,progressText, progressPercentage, isDayBarInverted );
+}
+
+function calculateDayProgressBarPercentage(inverted){
+  const currentTime = new Date();
+  const startHour = 6; // 6 AM
+  const endHour = 23; // 10 PM
+  const totalMinutes = (endHour - startHour) * 60;
+  const elapsedMinutes = currentTime.getHours()*60 + currentTime.getMinutes() - startHour * 60;
+  progressPercentage = (elapsedMinutes / totalMinutes) * 100;
+  if (inverted) {
+    progressPercentage = 100 - progressPercentage;
+  }
+  return progressPercentage;
+}
+
+
+let isCycleBarInverted = false;
+
+function toggleCycleBar() {
+  isCycleBarInverted = !isCycleBarInverted;
+  updateNavCycleTimer();
+  updateCycleProgressBar();
+}
+
+function updateNavCycleTimer() {                  
+  if (end_time_str) {
+      var end_time = new Date(end_time_str);
+      var remaining = Math.floor((end_time - new Date()) / 1000);
+      if (!isCycleBarInverted) {
+        remaining = 99*60*60+59*60+59 - remaining;
+      }
+      var hours = Math.floor(remaining / 3600);
+      var minutes = Math.floor((remaining % 3600) / 60).toString().padStart(2, '0');
+      var seconds = (remaining % 60).toString().padStart(2, '0');
+      var display = hours + ":" + minutes + ":" + seconds;
+      UpdateStringTimer("nav_cycle_lbl_time", display );
+  }
+}
+
+
+function calculateCycleProgressBarPercentage(inverted){
+  var end_time = new Date(end_time_str);
+  var remaining = Math.floor((end_time - new Date()) / 1000);
+  let progressPercentage = remaining/3600;
+
+  if (!inverted) {
+    progressPercentage = 100 - progressPercentage;
+  }
+  return progressPercentage;
+}
+
+
+
+function updateCycleProgressBar() {
+  const progressBar = document.getElementById('cycle-progress-bar');
+  const progressText = document.getElementById('cycle_progress-text');
+  progressPercentage = calculateCycleProgressBarPercentage(isCycleBarInverted);
+  updateProgressBar(progressBar,progressText, progressPercentage, isCycleBarInverted );
+}
+
+
+function updateProgressBar(progressBar, progressText, progressPercentage, isInverted) {
     const progressBarInner = progressBar.querySelector('.progress-bar');
-
-    const currentTime = new Date();
-    const startHour = 5; // 6 AM
-    const endHour = 22; // 10 PM
-
-    const totalMinutes = (endHour - startHour) * 60;
-    const elapsedMinutes = currentTime.getHours()*60 + currentTime.getMinutes() - startHour * 60;
-
-
-    let progressPercentage = (elapsedMinutes / totalMinutes) * 100;
     let internal_label = "Elapsed: ";
 
     if (isInverted) {
-        progressPercentage = 100 - progressPercentage;
         internal_label = "Remain: ";
     }
-    
 
+    if (progressPercentage < 5) {
+      internal_label='';
+    }
+    
     if (progressPercentage < 0) {
         progressPercentage = 0;
     } else if (progressPercentage > 100) {
@@ -101,6 +149,22 @@ function updateProgressBar(progressBar, progressText, isInverted) {
     progressBarInner.style.width = `${progressPercentageFixed}%`;
     progressText.textContent = internal_label + ` ${progressPercentageFixed}%`;
 }
+
+
+function toggleWorkingSession(){
+  event.preventDefault();
+  console.log("button");
+  fetch(`personal/toggle_work_session/` , {
+      method: 'GET',
+  });
+}
+
+function myFunction() {
+  // add your desired functionality here
+  console.log('Button clicked!');
+}
+
+
 
 
 
